c50948912f51bdc3a57f0bcceb751c76790215c6
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-20 17:29:30 +0100

    Fatta stilizzazione

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>PHP-dischi-json</title>
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="./css/style.css">
     <!-- vue 3 -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
@@ -17,15 +17,65 @@
 
 </head>
 
-<body>
+<body data-bs-theme="dark">
 
     <div id="app">
-    <div v-for="disk in disks" class="disk">
-            <img :src="disk.poster" alt="Poster">
-            <h2>{{ disk.title }}</h2>
-            <p>{{ disk.author }}</p>
-            <p>{{ disk.year }}</p>
-            <p>{{ disk.genre }}</p>
+        <nav class="navbar navbar-dark bg-dark fixed-top mb-5">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="./img/logo-small.svg" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-dark">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="d-flex mt-3" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="container p-5">
+            <div id="content-disk" class="d-flex flex-wrap">
+                <div v-for="disk in disks" id="disk" class="p-5 border rounded">
+                    <img :src="disk.poster" alt="Poster" class="mb-5">
+                    <h4>{{ disk.title }}</h4>
+                    <div id="center-text" class="d-flex align-items-center justify-content-center gap-2">
+                        <p>{{ disk.author }}</p>
+                        <p>{{ disk.year }}</p>
+
+                    </div>
+                    <p>{{ disk.genre }}</p>
+                </div>
+
+            </div>
+
         </div>
     </div>
 
